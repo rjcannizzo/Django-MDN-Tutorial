ffedf6333c6c6dc973d5138af5b62a40b5630447
rc <rc.info@protonmail.com>
2023-12-12 09:11:46 -0600

    working on chapter 6

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
-# Create your views here.
+class BookListView(generic.ListView):
+    model = Book
+
+
+def index(request):
+    """View function for home page of site."""
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+
