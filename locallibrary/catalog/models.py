c0dc3cdfd63a38cf65dff91ce8ed70b87d9966d9
rc <rc.info@protonmail.com>
2023-12-13 10:30:26 -0600

    added borrower to bookinstance class and did mmigration

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
+from datetime import date
 import uuid
 
 class Author(models.Model):
@@ -92,6 +94,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -114,5 +117,11 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
 
 
