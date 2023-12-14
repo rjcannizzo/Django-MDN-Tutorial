4d92c41429b678b1a46abc97e1fda532bb24817b
rc <rc.info@protonmail.com>
2023-12-14 09:24:32 -0600

    almost done with part 9

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('author/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
 
 
