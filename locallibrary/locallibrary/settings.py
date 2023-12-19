a502aa2da99a8e8d83134a765d1df8ce3df537a1
rc <rc.info@protonmail.com>
2023-12-19 08:35:55 -0600

    added Profile for Railway deployment but will create a new branch afterwards

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-igv4hd=yk+pilb)!ghg$agzrzhbb%i-_=l=$k+e1%66vi-+xkw'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-igv4hd=yk+pilb)!ghg$agzrzhbbxi-_=l=$k+e1%66vi-+xkw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
